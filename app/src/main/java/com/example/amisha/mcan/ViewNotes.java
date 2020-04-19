75b0cbfecc8c99e8982120adb044d4e203a8f99b
amisha-w <amishabw@gmail.com>
2020-04-19 18:28:59 +0530

    saving notes2

--- app/src/main/java/com/example/amisha/mcan/ViewNotes.java
+++ app/src/main/java/com/example/amisha/mcan/ViewNotes.java
@@ -2,26 +2,46 @@ package com.example.amisha.mcan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.ListActivity;
 import android.os.Bundle;
 import android.os.Environment;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
-public class ViewNotes extends ListActivity {
+public class ViewNotes extends Activity {
 
+    private ListView list;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_notes);
 
+        list = findViewById(R.id.list);
         File dir =  new File(Environment.getExternalStorageDirectory(),"Mcan");
         File[] filelist = dir.listFiles();
-        String[] theNamesOfFiles = new String[filelist.length];
-        for (int i = 0; i < theNamesOfFiles.length; i++) {
-            theNamesOfFiles[i] = filelist[i].getName();
+        List listNames = new ArrayList();
+        listNames.clear();
+        for (File f: dir.listFiles()){
+//            Date d = new Date(f.lastModified());
+            listNames.add(f.getName());
         }
-        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.activity_list_item, theNamesOfFiles));
+        list.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listNames));
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String name = (String) adapterView.getItemAtPosition(i);
+                Toast.makeText(ViewNotes.this, ""+name+" was clicked.", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
